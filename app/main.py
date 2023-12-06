a7f2093ce577a7ce94b23bdbb705e3e3a9d123c4
VarunSivamani <varunsivamani@gmail.com>
2023-12-06 19:54:36 +0530

    modified create_posts with ORM

--- app/main.py
+++ app/main.py
@@ -84,12 +84,16 @@ async def get_posts(db: Session = Depends(get_db)):
 
 
 @app.post("/createposts", status_code=status.HTTP_201_CREATED)
-async def create_posts(post: Post):
-    cursor.execute(""" INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, 
-                   (post.title, post.content, post.published))
-    new_post = cursor.fetchone()
-    conn.commit()
-    
+async def create_posts(post: Post, db: Session = Depends(get_db)):
+    # cursor.execute(""" INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, 
+    #                (post.title, post.content, post.published))
+    # new_post = cursor.fetchone()
+    # conn.commit()
+
+    new_post = models.Post(title=post.title, content=post.content, published=post.published)
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
     return {"data": new_post}
 
 

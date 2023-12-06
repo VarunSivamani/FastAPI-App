15dbb33f77793aa2e7a23b72ea842b69bc7701e0
VarunSivamani <varunsivamani@gmail.com>
2023-12-06 20:50:51 +0530

    modified get_post with ORM

--- app/main.py
+++ app/main.py
@@ -98,9 +98,11 @@ async def create_posts(post: Post, db: Session = Depends(get_db)):
 
 
 @app.get("/posts/{id}")
-def get_post(id: int, response: Response):
-    cursor.execute(""" SELECT * FROM posts WHERE id = %s """, (str(id)))
-    post = cursor.fetchone()
+def get_post(id: int, db: Session = Depends(get_db)):
+    # cursor.execute(""" SELECT * FROM posts WHERE id = %s """, (str(id)))
+    # post = cursor.fetchone()
+
+    post = db.query(models.Post).filter(models.Post.id == id).first()
 
     if not post:
         raise HTTPException(

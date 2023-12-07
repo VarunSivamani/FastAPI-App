ab5621265ca7dd49e86f8b063381d85e1875eb83
VarunSivamani <varunsivamani@gmail.com>
2023-12-07 16:21:47 +0530

    added JWT and oAuth2 functionalities

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ from psycopg2.extras import RealDictCursor
 import time
 from . import models
 from .database import engine
-from .routers import post, user
+from .routers import post, user, auth
 
 load_dotenv()
 
@@ -58,6 +58,7 @@ def find_index_post(id):
 
 app.include_router(post.router)
 app.include_router(user.router)
+app.include_router(auth.router)
 
 
 @app.get("/")

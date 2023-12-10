73cac1c3903551d747a64ef3d65742249612b442
VarunSivamani <varunsivamani@gmail.com>
2023-12-10 21:57:22 +0530

    added foreign key and other updates

--- app/oauth2.py
+++ app/oauth2.py
@@ -5,7 +5,8 @@ from fastapi import Response, status, HTTPException, Depends
 from fastapi.security import OAuth2PasswordBearer
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
-from . import schemas
+from . import schemas, database, models
+from sqlalchemy.orm import Session
 
 load_dotenv()
 
@@ -46,11 +47,15 @@ def verify_access_token(token: str, credentials_exception):
     return token_data
     
 
-def get_current_user(token: str = Depends(oauth2_scheme)):
+def get_current_user(token: str = Depends(oauth2_scheme), db: Session = Depends(database.get_db)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail=f"Could not validate credentials",
         headers={"WWW-Authenticate" : "Bearer"}
     )
 
-    return verify_access_token(token, credentials_exception)
+    token = verify_access_token(token, credentials_exception)
+
+    user = db.query(models.User.id == token.id).first()
+
+    return user
--- app/routers/post.py
+++ app/routers/post.py
@@ -21,13 +21,13 @@ async def get_posts(db: Session = Depends(get_db)):
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
 async def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db), 
-                       user_id: int = Depends(oauth2.get_current_user)):
+                       current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute(""" INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING * """, 
     #                (post.title, post.content, post.published))
     # new_post = cursor.fetchone()
     # conn.commit()
 
-    new_post = models.Post(**post.dict())
+    new_post = models.Post(owner_id=current_user.id, **post.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
@@ -36,7 +36,7 @@ async def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db),
 
 @router.get("/{id}", response_model=schemas.Post)
 def get_post(id: int, db: Session = Depends(get_db),
-             user_id: int = Depends(oauth2.get_current_user)):
+             current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute(""" SELECT * FROM posts WHERE id = %s """, (str(id)))
     # post = cursor.fetchone()
 
@@ -53,7 +53,7 @@ def get_post(id: int, db: Session = Depends(get_db),
 
 @router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int, db: Session = Depends(get_db),
-                user_id: int = Depends(oauth2.get_current_user)):
+                current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute(""" DELETE FROM posts WHERE id = %s RETURNING * """, (str(id)))
     # deleted_post = cursor.fetchone()
     # conn.commit()
@@ -74,7 +74,7 @@ def delete_post(id: int, db: Session = Depends(get_db),
 
 @router.put("/{id}", response_model=schemas.Post)
 def update_post(id: int, update_post: schemas.PostCreate, db: Session = Depends(get_db),
-                user_id: int = Depends(oauth2.get_current_user)):
+                current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute("""UPDATE posts SET title = %s, content = %s, published = %s WHERE id = %s RETURNING *""",
     #                (post.title, post.content, post.published, str(id)))
     

b392b3af625314cb72b4058b67a226c3bf3ac536
VarunSivamani <varunsivamani@gmail.com>
2023-12-11 21:40:19 +0530

    added Votes model, schemas, functionalities

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from dotenv import load_dotenv
 from fastapi import FastAPI
 from . import models
 from .database import engine
-from .routers import post, user, auth
+from .routers import post, user, auth, vote
 
 load_dotenv()
 
@@ -12,6 +12,7 @@ models.Base.metadata.create_all(bind=engine)
 app.include_router(post.router)
 app.include_router(user.router)
 app.include_router(auth.router)
+app.include_router(vote.router)
 
 
 @app.get("/")
--- app/routers/post.py
+++ app/routers/post.py
@@ -1,6 +1,6 @@
 from fastapi import Response, status, HTTPException, Depends, APIRouter
 from typing import List, Optional
-
+from sqlalchemy import func
 from sqlalchemy.orm import Session
 from .. import models, schemas, oauth2
 from ..database import get_db
@@ -10,14 +10,17 @@ router = APIRouter(
     tags = ["Posts"]
 )
 
-@router.get("/", response_model=List[schemas.Post])
+@router.get("/", response_model=List[schemas.PostOut])
 async def get_posts(db: Session = Depends(get_db),
                     current_user: int = Depends(oauth2.get_current_user), limit: int=10,
                     skip: int=0, search: Optional[str]=""):
     # cursor.execute(""" SELECT * FROM posts """)
     # posts = cursor.fetchall()
     
-    posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+    # posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
+    
+    posts = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(
+        models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
     return posts
 
 
@@ -36,7 +39,7 @@ async def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db),
     return new_post
 
 
-@router.get("/{id}", response_model=schemas.Post)
+@router.get("/{id}", response_model=schemas.PostOut)
 def get_post(id: int, db: Session = Depends(get_db),
              current_user: int = Depends(oauth2.get_current_user)):
     # cursor.execute(""" SELECT * FROM posts WHERE id = %s """, (str(id)))
